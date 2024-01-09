eec1863b1677b812d1f1769426dd7301c1dab48e
Fiama Alves <fiamaalves@alu.ufc.br>
2024-01-09 14:33:00 -0300

    Aula11
    Condicionais - JavaScript

---
+++ aula11/exe008.js
@@ -0,0 +1,6 @@
+var vel = 20.5
+
+if (vel>60){ //condição simples
+    console.log(`a velocidade do seu carro é ${vel} , você ultrapassou o limite. Multado!`)
+}
+console.log('Dirija sempre usando cinto de segurança')
